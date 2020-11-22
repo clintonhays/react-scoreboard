628db9ba43287cb8319fc604264944a2c49be7fa
clintonhays <clintonhays84@gmail.com>
2020-11-22 20:38:02 +0900

    add type checking

--- src/components/App.js
+++ src/components/App.js
@@ -68,7 +68,6 @@ class App extends Component {
       <div className="scoreboard">
         <Header
           /* prettier ignore */
-          title="Scoreboard"
           players={this.state.players}
         />
 
