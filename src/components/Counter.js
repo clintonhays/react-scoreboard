edd587dff62417ab5fb7ab7bce9231a6e42dd853
clintonhays <clintonhays84@gmail.com>
2020-11-22 18:45:32 +0900

    destructure props

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-const Counter = (props) => {
-	let index = props.index;
-
-	return (
-		<div className="counter">
-			<button className="counter-action decremement" onClick={() => props.changeScore(index, -1)}>
-				-
-			</button>
-			<span className="counter-score"> {props.score} </span>
-			<button className="counter-action incremement" onClick={() => props.changeScore(index, +1)}>
-				+
-			</button>
-		</div>
-	);
+const Counter = ({ index, score, changeScore }) => {
+  return (
+    <div className="counter">
+      <button className="counter-action decremement" onClick={() => changeScore(index, -1)}>
+        -
+      </button>
+      <span className="counter-score"> {score} </span>
+      <button className="counter-action incremement" onClick={() => changeScore(index, +1)}>
+        +
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
