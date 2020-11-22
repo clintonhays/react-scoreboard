3c329eba0b265507233c3eaf4c1c20fc64343538
clintonhays <clintonhays84@gmail.com>
2020-11-22 16:49:15 +0900

    add stats componenet to scoreboard header

--- src/components/App.js
+++ src/components/App.js
@@ -3,72 +3,72 @@ import Header from './Header';
 import Player from './Player';
 
 class App extends Component {
-	state = {
-		players : [
-			{
-				name  : 'Clinton',
-				score : 0,
-				id    : 1,
-			},
-			{
-				name  : 'Rhi',
-				score : 0,
-				id    : 2,
-			},
-			{
-				name  : 'Mark',
-				score : 0,
-				id    : 3,
-			},
-			{
-				name  : 'Amanda',
-				score : 0,
-				id    : 4,
-			},
-		],
-	};
+  state = {
+    players : [
+      {
+        name  : 'Clinton',
+        score : 0,
+        id    : 1,
+      },
+      {
+        name  : 'Rhi',
+        score : 0,
+        id    : 2,
+      },
+      {
+        name  : 'Mark',
+        score : 0,
+        id    : 3,
+      },
+      {
+        name  : 'Amanda',
+        score : 0,
+        id    : 4,
+      },
+    ],
+  };
 
-	handleScoreChange = (index, delta) => {
-		this.setState((prevState) => {
-			return {
-				score : (prevState.players[index].score += delta),
-			};
-		});
-	};
+  handleScoreChange = (index, delta) => {
+    this.setState((prevState) => {
+      return {
+        score : (prevState.players[index].score += delta),
+      };
+    });
+  };
 
-	handleRemovePlayer = (id) => {
-		this.setState((prevState) => {
-			return {
-				players : prevState.players.filter((player) => player.id !== id),
-			};
-		});
-	};
+  handleRemovePlayer = (id) => {
+    this.setState((prevState) => {
+      return {
+        players : prevState.players.filter((player) => player.id !== id),
+      };
+    });
+  };
 
-	render () {
-		return (
-			<div className="scoreboard">
-				<Header
-					/* prettier ignore */
-					title="Scoreboard"
-					totalPlayers={this.state.players.length}
-				/>
+  render () {
+    return (
+      <div className="scoreboard">
+        <Header
+          /* prettier ignore */
+          title="Scoreboard"
+          players={this.state.players}
+        />
 
-				{/* Player List */}
-				{this.state.players.map((player, index) => (
-					<Player
-						/* prettier ignore */
-						name={player.name}
-						score={player.score}
-						id={player.id}
-						key={player.id.toString()}
-						index={index}
-						changeScore={this.handleScoreChange}
-						removePlayer={this.handleRemovePlayer}
-					/>
-				))}
-			</div>
-		);
-	}
+        {/* Player List */}
+        {this.state.players.map((player, index) => (
+          <Player
+            /* prettier ignore */
+            name={player.name}
+            score={player.score}
+            id={player.id}
+            key={player.id.toString()}
+            index={index}
+            changeScore={this.handleScoreChange}
+            removePlayer={this.handleRemovePlayer}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
